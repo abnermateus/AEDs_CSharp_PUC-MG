01dd3109db2a869147c988c1001d7efe1fdefe2d
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2023-08-14 22:27:22 -0300

    commit

--- Lista_Nivelamento/Exercicios.cs
+++ Lista_Nivelamento/Exercicios.cs
@@ -44,7 +44,7 @@ namespace Lista_Nivelamento
             */
 
             /*Ex2
-             int numeroAtual, numeroAnterior = 0, sequencia = 0, maiorSequencia = 0;
+            int numeroAtual, numeroAnterior = 0, sequencia = 0, maiorSequencia = 0;
             Console.WriteLine("Informe uma sequência de números inteiros positivos (para finalizar, digite -1): ");
            
             do
@@ -68,22 +68,80 @@ namespace Lista_Nivelamento
             } while (numeroAtual != -1);
 
             Console.WriteLine("A maior sequência crescente possui {0} números", maiorSequencia);
+            */
+
+            /*Ex3
+            int numerador = 4, denominador = 1, N, sinal = 1;
+            double soma = 0;
+
+            Console.Write("Informe um valor limite para o denominador: ");
+            N = int.Parse(Console.ReadLine());
+
+            while (denominador <= N)
+            {
+                soma += sinal * (double)numerador / denominador;
+                Console.WriteLine("{0} / {1} = {2}", numerador, denominador, (double)numerador / denominador);
+                Console.WriteLine("Soma = {0}", soma);
+                sinal *= -1;
+                denominador += 2;
+            }
+            */
+
+            /*Ex4
+            int[] x = new int[5];
+            int[] y = new int[5];
+            int soma, produto;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write("Informe o valor de x{0}: ", i + 1);
+                x[i] = int.Parse(Console.ReadLine());
+                Console.Write("Informe o valor de y{0}: ", i + 1);
+                y[i] = int.Parse(Console.ReadLine());
+
+                soma = x[i] + y[i];
+                produto = x[i] * y[i];
+
+                Console.WriteLine("Soma: {0} + {1} = {2}", x[i], y[i], soma);
+                Console.WriteLine("Produto: {0} x {1} = {2}", x[i], y[i], produto);
+            }
+
+            Console.Write("Diferença: "); 
+            
+            for (int i = 0; i < x.Length; i++)
+            {
+                bool saoIguais = true;
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] != y[j])
+                    {
+                        saoIguais = false;
+                    }
+                }
+                if (!saoIguais)
+                {
+                    Console.Write("{0} ", x[i]);
+                }
+            }
+        
+            Console.Write("\nInterseção: ");
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                bool saoIguais = false;
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        saoIguais = true;
+                    }
+                }
+                if (saoIguais)
+                {
+                    Console.Write("{0} ", x[i]);
+                }
+            }
+            */
         }
-        */
-
-        int numerador = 4, denominador = 1, N;
-        double soma = 0;
-
-        Console.Write("Informe um valor ímpar limite para o denominador: ");
-        N = int.Parse(Console.ReadLine());
-
-
-        for (int i = 0; i < 20; i++)
-        {
-            soma += (double)numerador / denominador;
-            denominador += 2;
-        }
-
-
     }
 }
