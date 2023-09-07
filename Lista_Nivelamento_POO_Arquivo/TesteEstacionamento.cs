172570b6d1360e069ca0d181ac2a6d0b5ea9d638
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2023-09-07 12:36:49 -0300

    commit

--- Lista_Nivelamento_POO_Arquivo/TesteEstacionamento.cs
+++ Lista_Nivelamento_POO_Arquivo/TesteEstacionamento.cs
@@ -4,36 +4,38 @@ namespace Lista_Nivelamento_POO_Arquivo
 {
     public class TesteEstacionamento
     {
+        /*
         public static void Main(String[] args)
         {
             int vagaOcupada;
             Estacionamento objEstacionamento = new Estacionamento("Estacionamento de AEDs", 10);
-            vagaOcupada = objEstacionamento.estacionar("HKT0098");
-            vagaOcupada = objEstacionamento.estacionar("OLP4290");
-            vagaOcupada = objEstacionamento.estacionar("HJB0495");
-            vagaOcupada = objEstacionamento.estacionar("OWB3904");
+            vagaOcupada = objEstacionamento.Estacionar("HKT0098");
+            vagaOcupada = objEstacionamento.Estacionar("OLP4290");
+            vagaOcupada = objEstacionamento.Estacionar("HJB0495");
+            vagaOcupada = objEstacionamento.Estacionar("OWB3904");
 
             Console.WriteLine("Ocupação do estacionamento após as chegadas de quatro clientes: ");
-            objEstacionamento.exibirOcupacaoEstacionamento();
+            objEstacionamento.ExibirOcupacaoEstacionamento();
 
-            vagaOcupada = objEstacionamento.obterVagaOcupada("HKT0098");
+            vagaOcupada = objEstacionamento.ObterVagaOcupada("HKT0098");
             Console.WriteLine("Veículo de placa HKT0098 estacionado na vaga " + vagaOcupada);
 
-            objEstacionamento.retirarVeiculo("HKT0098");
+            objEstacionamento.RetirarVeiculo("HKT0098");
             Console.WriteLine("Ocupação do estacionamento após a retirada do veículo de placa HKT0098: ");
 
-            objEstacionamento.exibirOcupacaoEstacionamento();
+            objEstacionamento.ExibirOcupacaoEstacionamento();
 
-            vagaOcupada = objEstacionamento.estacionar("HTP5619");
-            vagaOcupada = objEstacionamento.estacionar("BOL4861");
-            vagaOcupada = objEstacionamento.estacionar("HGT9436");
+            vagaOcupada = objEstacionamento.Estacionar("HTP5619");
+            vagaOcupada = objEstacionamento.Estacionar("BOL4861");
+            vagaOcupada = objEstacionamento.Estacionar("HGT9436");
 
             Console.WriteLine("Ocupação do estacionamento após as chegadas de mais três clientes: ");
-            objEstacionamento.exibirOcupacaoEstacionamento();
+            objEstacionamento.ExibirOcupacaoEstacionamento();
 
-            Console.WriteLine("Este estacionamento apresenta {0} vagas livres.", objEstacionamento.obterNumVagasLivres());
+            Console.WriteLine("Este estacionamento apresenta {0} vagas livres.", objEstacionamento.ObterNumVagasLivres());
             Console.ReadLine();
         }
+        */
     }
 }
 
